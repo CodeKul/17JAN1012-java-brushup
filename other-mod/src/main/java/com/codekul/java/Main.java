1cb12c64a45fa9f40a89cd36bcde6672630babc9
aniruddha <aniruddha.kudalkar@gmail.com>
2017-01-24 12:32:53 +0530

    fatJar task

---
+++ other-mod/src/main/java/com/codekul/java/Main.java
@@ -0,0 +1,11 @@
+package com.codekul.java;
+
+/**
+ * Created by aniruddha on 24/1/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.printf("Hello from codekul :)");
+    }
+}
