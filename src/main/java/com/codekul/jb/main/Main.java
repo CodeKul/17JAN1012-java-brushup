5ce228d04498ebb8f70939dce3e3f7512b64437c
aniruddha <aniruddha.kudalkar@gmail.com>
2017-01-24 11:38:04 +0530

    initial setup

---
+++ src/main/java/com/codekul/jb/main/Main.java
@@ -0,0 +1,15 @@
+package com.codekul.jb.main;
+
+/**
+ * Created by aniruddha on 18/1/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void other(){
+
+    }
+}
